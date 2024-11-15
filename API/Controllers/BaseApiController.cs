840d87d44f1e31b085d4edf562d7ebbb350212d8
kimbit23 <kimberley.macdonald@gmail.com>
2024-11-15 18:10:27 -0400

    End of section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //localhost:5000/baseapi
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
